1f85fca96222f821d3efc22175b4a8f49b12659f
PatrickManalich <patrickmanalich@gmail.com>
2019-04-07 00:08:41 -0400

    Create AddButton and implement functionality with error handling

---
+++ Unity/Assets/Scripts/ScaleStats/AddButton.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CalorieCounter.ScaleStats {
+
+    public class AddButton : MonoBehaviour {
+
+        [SerializeField]
+        private ScrollView _scrollView;
+
+        [SerializeField]
+        private TextMeshProUGUI _errorText;
+
+        public void TryAddingInputFields() {
+            if (_scrollView.HasInputFields()) {
+                _errorText.text = "Input Fields Already Active";
+                _errorText.gameObject.SetActive(true);
+                return;
+            }
+
+            _errorText.gameObject.SetActive(false);
+            _scrollView.AddInputFields();
+        }
+
+        private void Start() {
+            _errorText.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/ScaleStats/ScrollView.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CalorieCounter.ScaleStats {
+
+    public class ScrollView : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject _scrollViewBlankPrefab;
+
+        [SerializeField]
+        private GameObject _scrollViewInputFieldPrefab;
+
+        [SerializeField]
+        private GridLayoutGroup _content;
+
+        public void AddInputFields() {
+            Instantiate(_scrollViewBlankPrefab, _content.transform);
+
+            for(int i = 0; i < _content.constraintCount - 1; i++) {
+                Instantiate(_scrollViewInputFieldPrefab, _content.transform);
+            }
+        }
+
+        public bool HasInputFields() {
+            foreach(Transform child in _content.transform) {
+                if(child.GetComponent<TMP_InputField>() != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
