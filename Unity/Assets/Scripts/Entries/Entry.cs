2d72b0aa595d38ee1534d706df9a822dcd98027f
PatrickManalich <patrickmanalich@gmail.com>
2019-04-07 11:41:20 -0400

    Create Entry struct and add entry to scroll view

---
+++ Unity/Assets/Scripts/Entries/Entry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalorieCounter.Entries {
+
+    public struct Entry {
+
+        public DateTime Date;
+        public float Weight;
+        public float BodyFat;
+        public float BodyWater;
+        public float MuscleMass;
+        public float BoneMass;
+        public float Bmi;
+
+        public Entry(float weight, float bodyFat, float bodyWater, float muscleMass, float boneMass, float bmi) {
+            Date = DateTime.Today;
+            Weight = weight > 0 ? Round(weight) : 0;
+            BodyFat = bodyFat > 0 ? Round(bodyFat) : 0;
+            BodyWater = bodyWater > 0 ? Round(bodyWater) : 0;
+            MuscleMass = muscleMass > 0 ? Round(muscleMass) : 0;
+            BoneMass = boneMass > 0 ? Round(boneMass) : 0;
+            Bmi = bmi > 0 ? Round(bmi) : 0;
+        }
+
+        public override string ToString() {
+            return Date + ", [ Weight:" + Weight + ", BodyFat:" + BodyFat + ", BodyWater:" + BodyWater + ", MuscleMass:" + MuscleMass + ", BoneMass:" + BoneMass + ", Bmi:" + Bmi + " ]";
+        }
+
+        private static float Round(float number) {
+            return (float)Math.Round(number, 1);
+        }
+    }
+}
--- Unity/Assets/Scripts/Entries/ScrollView.cs
+++ Unity/Assets/Scripts/Entries/ScrollView.cs
@@ -14,9 +14,14 @@ namespace CalorieCounter.Entries {
         private GameObject _scrollViewInputFieldPrefab;
 
         [SerializeField]
+        private GameObject _scrollViewTextPrefab;
+
+        [SerializeField]
         private GridLayoutGroup _content;
 
-        private List<TMP_InputField> _inputFields = new List<TMP_InputField>(); 
+        private List<TMP_InputField> _inputFields = new List<TMP_InputField>();
+
+        private List<Entry> _entries = new List<Entry>();
 
         public void AddInputFields() {
             Instantiate(_scrollViewBlankPrefab, _content.transform);
@@ -36,11 +41,41 @@ namespace CalorieCounter.Entries {
         }
 
         public void AddEntry() {
-            Debug.Log("Adding!");
+            GameObject dateText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject weightText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject bodyFatText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject bodyWaterText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject muscleMassText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject boneMassText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject bmiText = Instantiate(_scrollViewTextPrefab, _content.transform);
+
+            Entry entry = new Entry(float.Parse(_inputFields[0].text), float.Parse(_inputFields[1].text), float.Parse(_inputFields[2].text),
+                float.Parse(_inputFields[3].text), float.Parse(_inputFields[4].text), float.Parse(_inputFields[5].text));
+
+            for(int i = 0; i < _content.constraintCount; i++) {
+                int childIndex = (_entries.Count * _content.constraintCount) + i;
+                Destroy(_content.transform.GetChild(childIndex).gameObject);
+            }
+
+            dateText.GetComponent<TextMeshProUGUI>().text = entry.Date.ToString("MM/dd/yyyy");
+            weightText.GetComponent<TextMeshProUGUI>().text = entry.Weight.ToString();
+            bodyFatText.GetComponent<TextMeshProUGUI>().text = entry.BodyFat.ToString();
+            bodyWaterText.GetComponent<TextMeshProUGUI>().text = entry.BodyWater.ToString();
+            muscleMassText.GetComponent<TextMeshProUGUI>().text = entry.MuscleMass.ToString();
+            boneMassText.GetComponent<TextMeshProUGUI>().text = entry.BoneMass.ToString();
+            bmiText.GetComponent<TextMeshProUGUI>().text = entry.Bmi.ToString();
+
+            _inputFields.Clear();
+            _entries.Add(entry);
         }
 
         public bool HasInputFields() {
-            return _inputFields.Count > 0;
+            foreach (Transform child in _content.transform) {
+                if (child.GetComponent<TMP_InputField>() != null) {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool AllInputFieldsFilled() {
