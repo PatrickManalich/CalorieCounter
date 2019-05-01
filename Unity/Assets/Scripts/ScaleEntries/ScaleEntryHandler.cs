66d90bbcc0e37604fe33cce354710b7a6078a628
PatrickManalich <patrickmanalich@gmail.com>
2019-04-30 22:21:56 -0400

    [cosmetic] Rename ScaleEntryImporter -> ScaleEntryHandler

--- Unity/Assets/Scripts/ScaleEntries/ScaleEntryHandler.cs
+++ Unity/Assets/Scripts/ScaleEntries/ScaleEntryHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace CalorieCounter.ScaleEntries {
 
-    public class ScaleEntryImporter : MonoBehaviour {
+    public class ScaleEntryHandler : MonoBehaviour {
 
         [SerializeField]
         private ScrollView _scrollView = default;
