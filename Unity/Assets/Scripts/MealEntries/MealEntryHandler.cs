dd08f113535ad32d6f90879e6a502446228a09a2
PatrickManalich <patrickmanalich@gmail.com>
2019-05-13 22:00:33 -0400

    [refactor] Split up MealEntryHandler with Totals

--- Unity/Assets/Scripts/MealEntries/MealEntryHandler.cs
+++ Unity/Assets/Scripts/MealEntries/MealEntryHandler.cs
@@ -1,9 +1,7 @@
 ﻿using CalorieCounter.Globals;
-using CalorieCounter.TargetEntries;
 using RotaryHeart.Lib.SerializableDictionary;
 using System.Collections.Generic;
 using System.IO;
-using TMPro;
 using UnityEngine;
 
 namespace CalorieCounter.MealEntries {
@@ -14,31 +12,14 @@ namespace CalorieCounter.MealEntries {
         private class ScrollViewDictionary : SerializableDictionaryBase<MealTypes, ScrollView> { }
 
         [SerializeField]
-        private TargetEntryHandler _targetEntryHandler = default;
-
-        [SerializeField]
         private Date _date = default;
-
-        [SerializeField]
-        private TextMeshProUGUI _fatText = default;
-
-        [SerializeField]
-        private TextMeshProUGUI _carbsText = default;
-
-        [SerializeField]
-        private TextMeshProUGUI _proteinText = default;
-
-        [SerializeField]
-        private TextMeshProUGUI _caloriesText = default;
 
         [SerializeField]
         private ScrollViewDictionary _scrollViewDict = default;
 
-        private float _totalFat = 0;
-        private float _totalCarbs = 0;
-        private float _totalProtein = 0;
-        private float _totalCalories = 0;
-        private TargetEntry _targetEntry;
+        [SerializeField]
+        private Totals _totals = default;
+
         private Dictionary<MealTypes, List<MealProportion>> _mealProportionsDict = new Dictionary<MealTypes, List<MealProportion>>() {
             { MealTypes.Small, new List<MealProportion>() },
             { MealTypes.Large, new List<MealProportion>() },
@@ -46,36 +27,26 @@ namespace CalorieCounter.MealEntries {
 
         public void AddMealProportion(MealProportion mealProportion) {
             _mealProportionsDict[mealProportion.Source.MealType].Add(mealProportion);
-            _totalFat += mealProportion.Fat;
-            _totalCarbs += mealProportion.Carbs;
-            _totalProtein += mealProportion.Protein;
-            _totalCalories += mealProportion.Calories;
-            RefreshTexts();
+            _totals.AddToTotals(mealProportion);
         }
 
         public void SubtractMealProportion(MealProportion mealProportion) {
             _mealProportionsDict[mealProportion.Source.MealType].Remove(mealProportion);
-            _totalFat -= mealProportion.Fat;
-            _totalCarbs -= mealProportion.Carbs;
-            _totalProtein -= mealProportion.Protein;
-            _totalCalories -= mealProportion.Calories;
-            RefreshTexts();
+            _totals.RemoveFromTotals(mealProportion);
         }
 
         public void ExportMealEntry() {
-            MealEntry currentMealEntry = new MealEntry(_date.CurrentDate, _totalFat, _totalCarbs, _totalProtein, _totalCalories, _mealProportionsDict);
+            MealEntry currentMealEntry = new MealEntry(_date.CurrentDate, _totals.TotalFat, _totals.TotalCarbs, _totals.TotalProtein, 
+                _totals.TotalCalories, _mealProportionsDict);
             JsonUtility.Export(currentMealEntry, GetMealEntryPath());
         }
 
-        public void RefreshMealAndTargetEntry() {
-            _totalFat = 0;
-            _totalCarbs = 0;
-            _totalProtein = 0;
-            _totalCalories = 0;
+        public void Refresh() {
             foreach (var key in _mealProportionsDict.Keys) {
                 _mealProportionsDict[key].Clear();
                 _scrollViewDict[key].ClearMealProportions();
             }
+            _totals.Refresh();
 
             MealEntry importedMealEntry = JsonUtility.Import<MealEntry>(GetMealEntryPath());
             if (importedMealEntry != default) {
@@ -87,31 +58,16 @@ namespace CalorieCounter.MealEntries {
                     }
                 }
             }
-
-            _targetEntry = _targetEntryHandler.GetLatestTargetEntry(_date.CurrentDate);
-            RefreshTexts();
         }
 
         private void Start() {
-            RefreshMealAndTargetEntry();
+            Refresh();
         }
 
         private string GetMealEntryPath() {
             string mealEntryFileDate = "-" + _date.CurrentDate.Year + "-" + _date.CurrentDate.Month + "-" + _date.CurrentDate.Day;
             string mealEntryFileName = GlobalPaths.MealEntryFilePrefix + mealEntryFileDate + GlobalPaths.MealEntryFileExtension;
             return Path.Combine(GlobalPaths.MealEntriesDir, mealEntryFileName);
-        }
-
-        private void RefreshTexts() {
-            _totalFat = GlobalMethods.Round(_totalFat);
-            _totalCarbs = GlobalMethods.Round(_totalCarbs);
-            _totalProtein = GlobalMethods.Round(_totalProtein);
-            _totalCalories = GlobalMethods.Round(_totalCalories);
-
-            _fatText.text = _totalFat.ToString() + " / " + _targetEntry.TrainingDayFatGrams;
-            _carbsText.text = _totalCarbs.ToString() + " / " + _targetEntry.TrainingDayCarbGrams;
-            _proteinText.text = _totalProtein.ToString() + " / " + _targetEntry.TrainingDayProteinGrams;
-            _caloriesText.text = _totalCalories.ToString() + " / " + _targetEntry.TrainingDayCalories;
         }
     }
 }
---
+++ Unity/Assets/Scripts/MealEntries/Totals.cs
@@ -0,0 +1,72 @@
+﻿using CalorieCounter.Globals;
+using CalorieCounter.TargetEntries;
+using TMPro;
+using UnityEngine;
+
+namespace CalorieCounter.MealEntries {
+
+    public class Totals : MonoBehaviour {
+
+        [SerializeField]
+        private TargetEntryHandler _targetEntryHandler = default;
+
+        [SerializeField]
+        private Date _date = default;
+
+        [SerializeField]
+        private TextMeshProUGUI _fatText = default;
+
+        [SerializeField]
+        private TextMeshProUGUI _carbsText = default;
+
+        [SerializeField]
+        private TextMeshProUGUI _proteinText = default;
+
+        [SerializeField]
+        private TextMeshProUGUI _caloriesText = default;
+
+        public float TotalFat { get; private set; } = 0;
+        public float TotalCarbs { get; private set; } = 0;
+        public float TotalProtein { get; private set; } = 0;
+        public float TotalCalories { get; private set; } = 0;
+
+        private TargetEntry _targetEntry;
+
+        public void AddToTotals(MealProportion mealProportion) {
+            TotalFat += mealProportion.Fat;
+            TotalCarbs += mealProportion.Carbs;
+            TotalProtein += mealProportion.Protein;
+            TotalCalories += mealProportion.Calories;
+            UpdateTexts();
+        }
+
+        public void RemoveFromTotals(MealProportion mealProportion) {
+            TotalFat -= mealProportion.Fat;
+            TotalCarbs -= mealProportion.Carbs;
+            TotalProtein -= mealProportion.Protein;
+            TotalCalories -= mealProportion.Calories;
+            UpdateTexts();
+        }
+
+        public void Refresh() {
+            TotalFat = 0;
+            TotalCarbs = 0;
+            TotalProtein = 0;
+            TotalCalories = 0;
+            _targetEntry = _targetEntryHandler.GetLatestTargetEntry(_date.CurrentDate);
+            UpdateTexts();
+        }
+
+        private void UpdateTexts() {
+            TotalFat = GlobalMethods.Round(TotalFat);
+            TotalCarbs = GlobalMethods.Round(TotalCarbs);
+            TotalProtein = GlobalMethods.Round(TotalProtein);
+            TotalCalories = GlobalMethods.Round(TotalCalories);
+
+            _fatText.text = TotalFat.ToString() + " / " + _targetEntry.TrainingDayFatGrams;
+            _carbsText.text = TotalCarbs.ToString() + " / " + _targetEntry.TrainingDayCarbGrams;
+            _proteinText.text = TotalProtein.ToString() + " / " + _targetEntry.TrainingDayProteinGrams;
+            _caloriesText.text = TotalCalories.ToString() + " / " + _targetEntry.TrainingDayCalories;
+        }
+    }
+}
