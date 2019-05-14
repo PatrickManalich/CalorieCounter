23d0ae153a94b48c8c52b8b7067052d6a6a9be9a
PatrickManalich <patrickmanalich@gmail.com>
2019-05-13 23:29:22 -0400

    Add AddButton and ScrollView for adding input fields

---
+++ Unity/Assets/Scripts/MealEntries/CustomMeals/AddButton.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CalorieCounter.MealEntries.CustomMeals {
+
+    public class AddButton : MonoBehaviour {
+
+        [SerializeField]
+        private ScrollView _scrollView = default;
+
+        [SerializeField]
+        private TextMeshProUGUI _errorText = default;
+
+        public void TryAddingInputFields() {
+            if (_scrollView.HasInputFields()) {
+                _errorText.text = "Input Fields Already Active";
+                _errorText.gameObject.SetActive(true);
+                return;
+            }
+
+            _errorText.gameObject.SetActive(false);
+            _scrollView.AddInputFields();
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/MealEntries/CustomMeals/ScrollView.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CalorieCounter.MealEntries.CustomMeals {
+
+    public class ScrollView : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject _scrollViewBlankPrefab = default;
+
+        [SerializeField]
+        private GameObject _scrollViewInputFieldPrefab = default;
+
+        [SerializeField]
+        private GridLayoutGroup _content = default;
+
+        private List<TMP_InputField> _inputFields = new List<TMP_InputField>();
+
+        public void AddInputFields() {
+            GameObject previous = null;
+            for (int i = 0; i < _content.constraintCount; i++) {
+                if (i == 0) {
+                    previous = Instantiate(_scrollViewInputFieldPrefab, _content.transform);
+                    previous.GetComponent<TMP_InputField>().ActivateInputField();
+                    _inputFields.Add(previous.GetComponent<TMP_InputField>());
+                } else if(i >= 2 && i <= 4) {
+                    GameObject current = Instantiate(_scrollViewInputFieldPrefab, _content.transform);
+                    previous.GetComponent<Tabbable>().NextSelectable = current.GetComponent<Selectable>();
+                    previous = current;
+                    _inputFields.Add(previous.GetComponent<TMP_InputField>());
+                } else {
+                    Instantiate(_scrollViewBlankPrefab, _content.transform);
+                }
+            }
+        }
+
+        public bool HasInputFields() {
+            foreach (Transform child in _content.transform) {
+                if (child.GetComponent<TMP_InputField>() != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
