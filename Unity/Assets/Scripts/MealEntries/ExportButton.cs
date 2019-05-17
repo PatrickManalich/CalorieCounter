bcfcb9083b201d85b32f87d5966cabee949eddc8
PatrickManalich <patrickmanalich@gmail.com>
2019-05-16 20:46:27 -0400

    Check DayType before exporting

--- Unity/Assets/Scripts/MealEntries/ExportButton.cs
+++ Unity/Assets/Scripts/MealEntries/ExportButton.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using CalorieCounter.Globals;
+using TMPro;
+using UnityEngine;
 
 namespace CalorieCounter.MealEntries {
 
@@ -7,7 +9,20 @@ namespace CalorieCounter.MealEntries {
         [SerializeField]
         private MealEntryHandler _mealEntryHandler = default;
 
+        [SerializeField]
+        private DayTypeDropdown _dayTypeDropdown = default;
+
+        [SerializeField]
+        private TextMeshProUGUI _errorText = default;
+
         public void TryExporting() {
+            if (_dayTypeDropdown.DayType == DayTypes.None) {
+                _errorText.text = "Select Valid Meal Type";
+                _errorText.gameObject.SetActive(true);
+                return;
+            }
+
+            _errorText.gameObject.SetActive(false);
             _mealEntryHandler.ExportMealEntry();
             gameObject.SetActive(false);
         }
