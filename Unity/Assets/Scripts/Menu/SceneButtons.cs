a89847e549e1e9b345a7d873fb326884bae0b01d
PatrickManalich <patrickmanalich@gmail.com>
2019-06-29 23:26:21 -0400

    Create SceneButtons that load scenes

---
+++ Unity/Assets/Scripts/Menu/SceneButtons.cs
@@ -0,0 +1,22 @@
+﻿using CalorieCounter.Managers;
+using RotaryHeart.Lib.SerializableDictionary;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CalorieCounter.Menu {
+
+    public class SceneButtons: MonoBehaviour {
+
+        [System.Serializable]
+        private class SceneButtonDictionary : SerializableDictionaryBase<GameObject, Scene> { }
+
+        [SerializeField]
+        private SceneButtonDictionary _sceneButtonDictionary = default;
+
+        private void Start() {
+            foreach (var key in _sceneButtonDictionary.Keys) {
+                key.GetComponent<Button>().onClick.AddListener(() => GameManager.CustomSceneManager.LoadScene(_sceneButtonDictionary[key]));
+            }
+        }
+    }
+}
