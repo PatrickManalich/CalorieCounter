c7e3a6e107ead320bc262b5b02d9b9c066edf375
PatrickManalich <patrickmanalich@gmail.com>
2019-07-30 21:10:06 -0400

    Add scripts to MealSources namespace

--- Unity/Assets/Scripts/MealSources/MealSourcesAdapter.cs
+++ Unity/Assets/Scripts/MealSources/MealSourcesAdapter.cs
@@ -1,54 +1,60 @@
-﻿using CalorieCounter;
-using CalorieCounter.Managers;
-using CalorieCounter.MealSources;
+﻿using CalorieCounter.Managers;
 using RotaryHeart.Lib.SerializableDictionary;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MealSourcesAdapter : MonoBehaviour
-{
-    [SerializeField]
-    private Scene _scene = default;
+namespace CalorieCounter.MealSources {
 
-    [System.Serializable]
-    private class ScrollViewDictionary : SerializableDictionaryBase<MealSourceType, MealSourcesScrollView> { }
+    public class MealSourcesAdapter : MonoBehaviour {
 
-    [DisplayBasedOnEnum("_scene", (int)Scene.MealSources)]
-    [SerializeField]
-    private ScrollViewDictionary _scrollViewDictionary = default;
+        [SerializeField]
+        private Scene _scene = default;
 
-    public List<MealSource> GetMealSources(MealSourceType mealSourceType)
-    {
-        var importedMealSourcesDictionary = GameManager.MealSourcesManager.ImportMealSourcesDictionary();
-        var mealSources = new List<MealSource>();
-        foreach(var pair in importedMealSourcesDictionary[mealSourceType])
+        [System.Serializable]
+        private class ScrollViewDictionary : SerializableDictionaryBase<MealSourceType, MealSourcesScrollView> { }
+
+        [DisplayBasedOnEnum("_scene", (int)Scene.MealSources)]
+        [SerializeField]
+        private ScrollViewDictionary _scrollViewDictionary = default;
+
+        public List<MealSource> GetMealSources(MealSourceType mealSourceType)
         {
-            mealSources.Add(pair.Value);
-        }
-        return mealSources;
-    }
-
-    public void ExportMealSourcesDictionary() {
-        var mealSourcesDictionary = new Dictionary<MealSourceType, SortedList<string, MealSource>>() {
-            { MealSourceType.Small, _scrollViewDictionary[MealSourceType.Small].MealSources },
-            { MealSourceType.Large, _scrollViewDictionary[MealSourceType.Large].MealSources },
-        };
-        GameManager.MealSourcesManager.ExportMealSourcesDictionary(mealSourcesDictionary);
-    }
-
-    private void Start() {
-        if (_scene != Scene.MealSources)
-        {
-            return;
+            var importedMealSourcesDictionary = GameManager.MealSourcesManager.ImportMealSourcesDictionary();
+            var mealSources = new List<MealSource>();
+            foreach (var pair in importedMealSourcesDictionary[mealSourceType])
+            {
+                mealSources.Add(pair.Value);
+            }
+            return mealSources;
         }
 
-        var importedMealSourcesDictionary = GameManager.MealSourcesManager.ImportMealSourcesDictionary();
-        if (importedMealSourcesDictionary != default) {
-            foreach (var key in importedMealSourcesDictionary.Keys) {
-                var mealSourcesScrollView = _scrollViewDictionary[key];
-                var importedMealSources = importedMealSourcesDictionary[key];
-                foreach (var mealSource in importedMealSources.Values) {
-                    mealSourcesScrollView.AddMealSource(mealSource);
+        public void ExportMealSourcesDictionary()
+        {
+            var mealSourcesDictionary = new Dictionary<MealSourceType, SortedList<string, MealSource>>() {
+                { MealSourceType.Small, _scrollViewDictionary[MealSourceType.Small].MealSources },
+                { MealSourceType.Large, _scrollViewDictionary[MealSourceType.Large].MealSources },
+            };
+            GameManager.MealSourcesManager.ExportMealSourcesDictionary(mealSourcesDictionary);
+        }
+
+        private void Start()
+        {
+            if (_scene != Scene.MealSources)
+            {
+                return;
+            }
+
+            var importedMealSourcesDictionary = GameManager.MealSourcesManager.ImportMealSourcesDictionary();
+            if (importedMealSourcesDictionary != default)
+            {
+                foreach (var key in importedMealSourcesDictionary.Keys)
+                {
+                    var mealSourcesScrollView = _scrollViewDictionary[key];
+                    var importedMealSources = importedMealSourcesDictionary[key];
+                    foreach (var mealSource in importedMealSources.Values)
+                    {
+                        mealSourcesScrollView.AddMealSource(mealSource);
+                    }
                 }
             }
         }
--- Unity/Assets/Scripts/MealSources/MealSourcesScrollView.cs
+++ Unity/Assets/Scripts/MealSources/MealSourcesScrollView.cs
@@ -1,53 +1,57 @@
-﻿using CalorieCounter.MealSources;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MealSourcesScrollView : MonoBehaviour
-{
+namespace CalorieCounter.MealSources {
 
-    [SerializeField]
-    private GameObject _scrollViewTextPrefab = default;
+    public class MealSourcesScrollView : MonoBehaviour
+    {
 
-    [SerializeField]
-    private GridLayoutGroup _content = default;
+        [SerializeField]
+        private GameObject _scrollViewTextPrefab = default;
 
-    [SerializeField]
-    private MealSourceInputFields _mealSourceInputFields = default;
+        [SerializeField]
+        private GridLayoutGroup _content = default;
 
-    public SortedList<string, MealSource> MealSources { get; private set; } = new SortedList<string, MealSource>();
+        [SerializeField]
+        private MealSourceInputFields _mealSourceInputFields = default;
 
-    public void AddMealSourceFromInputFields() {
-        AddMealSource(_mealSourceInputFields.GetMealSourceFromInputFields());
-    }
+        public SortedList<string, MealSource> MealSources { get; private set; } = new SortedList<string, MealSource>();
 
-    public void AddMealSource(MealSource mealSource) {
-        MealSources.Add(mealSource.Name, mealSource);
+        public void AddMealSourceFromInputFields()
+        {
+            AddMealSource(_mealSourceInputFields.GetMealSourceFromInputFields());
+        }
 
-        GameObject mealNameText = Instantiate(_scrollViewTextPrefab, _content.transform);
-        GameObject servingSizeText = Instantiate(_scrollViewTextPrefab, _content.transform);
-        GameObject fatText = Instantiate(_scrollViewTextPrefab, _content.transform);
-        GameObject carbText = Instantiate(_scrollViewTextPrefab, _content.transform);
-        GameObject proteinText = Instantiate(_scrollViewTextPrefab, _content.transform);
-        GameObject calorieText = Instantiate(_scrollViewTextPrefab, _content.transform);
-        GameObject descriptionText = Instantiate(_scrollViewTextPrefab, _content.transform);
+        public void AddMealSource(MealSource mealSource)
+        {
+            MealSources.Add(mealSource.Name, mealSource);
 
-        int siblingStartIndex = MealSources.IndexOfKey(mealSource.Name) * _content.constraintCount;
-        mealNameText.transform.SetSiblingIndex(siblingStartIndex);
-        servingSizeText.transform.SetSiblingIndex(siblingStartIndex + 1);
-        fatText.transform.SetSiblingIndex(siblingStartIndex + 2);
-        carbText.transform.SetSiblingIndex(siblingStartIndex + 3);
-        proteinText.transform.SetSiblingIndex(siblingStartIndex + 4);
-        calorieText.transform.SetSiblingIndex(siblingStartIndex + 5);
-        descriptionText.transform.SetSiblingIndex(siblingStartIndex + 6);
+            GameObject mealNameText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject servingSizeText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject fatText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject carbText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject proteinText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject calorieText = Instantiate(_scrollViewTextPrefab, _content.transform);
+            GameObject descriptionText = Instantiate(_scrollViewTextPrefab, _content.transform);
 
-        mealNameText.GetComponent<TextMeshProUGUI>().text = mealSource.Name;
-        servingSizeText.GetComponent<TextMeshProUGUI>().text = mealSource.ServingSize;
-        fatText.GetComponent<TextMeshProUGUI>().text = mealSource.Fat.ToString();
-        carbText.GetComponent<TextMeshProUGUI>().text = mealSource.Carbs.ToString();
-        proteinText.GetComponent<TextMeshProUGUI>().text = mealSource.Protein.ToString();
-        calorieText.GetComponent<TextMeshProUGUI>().text = mealSource.Calories.ToString();
-        descriptionText.GetComponent<TextMeshProUGUI>().text = mealSource.Description;
+            int siblingStartIndex = MealSources.IndexOfKey(mealSource.Name) * _content.constraintCount;
+            mealNameText.transform.SetSiblingIndex(siblingStartIndex);
+            servingSizeText.transform.SetSiblingIndex(siblingStartIndex + 1);
+            fatText.transform.SetSiblingIndex(siblingStartIndex + 2);
+            carbText.transform.SetSiblingIndex(siblingStartIndex + 3);
+            proteinText.transform.SetSiblingIndex(siblingStartIndex + 4);
+            calorieText.transform.SetSiblingIndex(siblingStartIndex + 5);
+            descriptionText.transform.SetSiblingIndex(siblingStartIndex + 6);
+
+            mealNameText.GetComponent<TextMeshProUGUI>().text = mealSource.Name;
+            servingSizeText.GetComponent<TextMeshProUGUI>().text = mealSource.ServingSize;
+            fatText.GetComponent<TextMeshProUGUI>().text = mealSource.Fat.ToString();
+            carbText.GetComponent<TextMeshProUGUI>().text = mealSource.Carbs.ToString();
+            proteinText.GetComponent<TextMeshProUGUI>().text = mealSource.Protein.ToString();
+            calorieText.GetComponent<TextMeshProUGUI>().text = mealSource.Calories.ToString();
+            descriptionText.GetComponent<TextMeshProUGUI>().text = mealSource.Description;
+        }
     }
 }
