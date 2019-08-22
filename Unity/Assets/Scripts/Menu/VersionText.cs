6a4a505fe3325cbc11b94139a431afb79c317d0b
PatrickManalich <patrickmanalich@gmail.com>
2019-08-21 23:09:29 -0400

    Add VersionText to GameManager

---
+++ Unity/Assets/Scripts/Menu/VersionText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace CalorieCounter.Menu
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class VersionText : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<TextMeshProUGUI>().text = Application.version;
+        }
+
+    }
+}
