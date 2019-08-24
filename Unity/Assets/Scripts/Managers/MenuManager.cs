5f94b69941feb2b953a5daa3a3ea3bd3ec822aba
PatrickManalich <patrickmanalich@gmail.com>
2019-08-24 11:36:16 -0400

    [refactor] Let MenuManager handle SceneButtons and VersionText

--- Unity/Assets/Scripts/Managers/MenuManager.cs
+++ Unity/Assets/Scripts/Managers/MenuManager.cs
@@ -1,13 +1,25 @@
-﻿using UnityEngine;
+﻿using RotaryHeart.Lib.SerializableDictionary;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace CalorieCounter.Managers {
 
     public class MenuManager : MonoBehaviour {
 
+        [System.Serializable]
+        private class SceneButtonDictionary : SerializableDictionaryBase<GameObject, Scene> { }
+
         [SerializeField]
         private GameObject _menuCanvas = default;
 
-        private KeyCode _menuKeyCode = KeyCode.Escape;
+        [SerializeField]
+        private TextMeshProUGUI _versionText = default;
+
+        [SerializeField]
+        private SceneButtonDictionary _sceneButtonDictionary = default;
+
+        private static readonly KeyCode _menuKeyCode = KeyCode.Escape;
 
         public void ShowMenu() {
             _menuCanvas.SetActive(true);
@@ -15,6 +27,17 @@ namespace CalorieCounter.Managers {
 
         public void HideMenu() {
             _menuCanvas.SetActive(false);
+        }
+
+        private void Start()
+        {
+            foreach (var key in _sceneButtonDictionary.Keys)
+            {
+                var button = key.GetComponent<Button>();
+                button.onClick.AddListener(() => GameManager.CustomSceneManager.LoadScene(_sceneButtonDictionary[key]));
+                button.onClick.AddListener(() => GameManager.MenuManager.HideMenu());
+            }
+            _versionText.text = Application.version;
         }
 
         private void Update() {
