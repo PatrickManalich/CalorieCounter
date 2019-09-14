760c9fe6fbd4d174835192ca413f3f53d1b265c0
PatrickManalich <patrickmanalich@gmail.com>
2019-09-14 13:28:29 -0400

    Removed unused CopyPasteAllComponents and add DirtySaveAllOpenScenes extension

---
+++ Unity/Assets/Scripts/Editor/EditorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace CalorieCounter
+{
+    public static class EditorExtensions
+    {
+
+        [MenuItem("Calorie Counter/Dirty Save Active Scene %#d")]
+        public static void DirtySaveAllOpenScenes()
+        {
+            if (Application.isPlaying)
+                return;
+
+            for (int i = 0; i < EditorSceneManager.sceneCount; i++)
+            {
+                EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetSceneAt(i));
+                EditorSceneManager.SaveScene(EditorSceneManager.GetSceneAt(i));
+            }
+        }
+    }
+}
