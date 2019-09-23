20dfb181f371734b913e43a63ce1cb01b583df7c
PatrickManalich <patrickmanalich@gmail.com>
2019-09-22 22:32:17 -0400

    Add old structs from release 1.0.0

---
+++ Unity/Assets/Scripts/Structs 1.0.0/MealEntry.cs
@@ -0,0 +1,54 @@
+﻿using CalorieCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalorieCounterOld.MealEntries {
+
+    public struct MealEntry {
+
+        public DateTime DateTime;
+        public DayType DayType;
+        public float TotalFat;
+        public float TotalCarbs;
+        public float TotalProtein;
+        public float TotalCalories;
+        public Dictionary<MealSourceType, List<MealProportion>> MealProportionsDictionary;
+
+        public MealEntry(DateTime dateTime, DayType dayType, float totalFat, float totalCarbs, float totalProtein, float totalCalories, Dictionary<MealSourceType, List<MealProportion>> mealProportionsDict) {
+            DateTime = dateTime;
+            DayType = dayType;
+            TotalFat = GlobalMethods.Round(totalFat);
+            TotalCarbs = GlobalMethods.Round(totalCarbs);
+            TotalProtein = GlobalMethods.Round(totalProtein);
+            TotalCalories = GlobalMethods.Round(totalCalories);
+            MealProportionsDictionary = mealProportionsDict;
+        }
+
+        public static bool operator ==(MealEntry mealEntry1, MealEntry mealEntry2) {
+            return mealEntry1.Equals(mealEntry2);
+        }
+
+        public static bool operator !=(MealEntry mealEntry1, MealEntry mealEntry2) {
+            return !mealEntry1.Equals(mealEntry2);
+        }
+
+        public override bool Equals(object obj) {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public override string ToString() {
+            int mealProportionsDictCount = 0;
+            if (MealProportionsDictionary != null) {
+                foreach(var mealSourceType in Enum.GetValues(typeof(MealSourceType)).Cast<MealSourceType>().ToList()) {
+                    mealProportionsDictCount += MealProportionsDictionary[mealSourceType].Count;
+                }
+            }
+            return string.Format("Date: {0}, Day Type: {1}, [ Total Fat: {2}, Total Carbs: {3}, Total Protein: {4}, Total Calories: {5} ], Meal Proportions Dict Count: {6}",
+                DateTime.ToShortDateString(), DayType, TotalFat, TotalCarbs, TotalProtein, TotalCalories, mealProportionsDictCount);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Structs 1.0.0/ScaleEntry.cs
@@ -0,0 +1,31 @@
+﻿using CalorieCounter;
+using System;
+
+namespace CalorieCounterOld.ScaleEntries {
+
+    public struct ScaleEntry {
+
+        public DateTime Date;
+        public float Weight;
+        public float BodyFat;
+        public float BodyWater;
+        public float MuscleMass;
+        public float BoneMass;
+        public float Bmi;
+
+        public ScaleEntry(DateTime date, float weight, float bodyFat, float bodyWater, float muscleMass, float boneMass, float bmi) {
+            Date = date;
+            Weight = weight > 0 ? GlobalMethods.Round(weight) : 0;
+            BodyFat = bodyFat > 0 ? GlobalMethods.Round(bodyFat) : 0;
+            BodyWater = bodyWater > 0 ? GlobalMethods.Round(bodyWater) : 0;
+            MuscleMass = muscleMass > 0 ? GlobalMethods.Round(muscleMass) : 0;
+            BoneMass = boneMass > 0 ? GlobalMethods.Round(boneMass) : 0;
+            Bmi = bmi > 0 ? GlobalMethods.Round(bmi) : 0;
+        }
+
+        public override string ToString() {
+            return string.Format("Date: {0}, Weight: {1}, Body Fat: {2}, Body Water: {3}, Muscle Mass: {4}, Bone Mass: {5}, BMI: {5}",
+                Date.ToShortDateString(), Weight, BodyFat, BodyWater, MuscleMass, BoneMass, Bmi);
+        }
+    }
+}
