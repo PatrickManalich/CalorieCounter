41dd0c632f40cf39d138ff0bf007a8105c6ca4f8
PatrickManalich <patrickmanalich@gmail.com>
2019-09-28 22:49:18 -0400

    Add NutritionixButton

---
+++ Unity/Assets/Scripts/MealEntries/NutritionixButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CalorieCounter.MealEntries
+{
+
+    [RequireComponent(typeof(Button))]
+    public class NutritionixButton : MonoBehaviour
+    {
+
+        private const string NutritionixUrl = "https://www.nutritionix.com/";
+
+        private Button _button;
+
+        private void Start()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(() => Application.OpenURL(NutritionixUrl));
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(() => Application.OpenURL(NutritionixUrl));
+        }
+    }
+
+}
