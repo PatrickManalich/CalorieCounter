f90c044eb69df695f5af7d34d3be54fee57a594a
PatrickManalich <patrickmanalich@gmail.com>
2019-10-05 17:48:28 -0400

    Create ExportCsvEditorExtensions and CsvWriterRecords

---
+++ Unity/Assets/Scripts/Editor/CsvWriterRecords.cs
@@ -0,0 +1,29 @@
+﻿
+using CalorieCounter.ScaleEntries;
+
+namespace CalorieCounter.EditorExtensions
+{
+
+    public class ScaleEntryRecord
+    {
+        public string Date { get; set; }
+        public float Weight { get; set; }
+        public float BodyFat { get; set; }
+        public float BodyWater { get; set; }
+        public float MuscleMass { get; set; }
+        public float BoneMass { get; set; }
+        public float Bmi { get; set; }
+
+        public ScaleEntryRecord(ScaleEntry scaleEntry)
+        {
+            Date = scaleEntry.date.ToShortDateString();
+            Weight = scaleEntry.weight;
+            BodyFat = scaleEntry.bodyFat;
+            BodyWater = scaleEntry.bodyWater;
+            MuscleMass = scaleEntry.muscleMass;
+            BoneMass = scaleEntry.boneMass;
+            Bmi = scaleEntry.bmi;
+        }
+    }
+
+}
---
+++ Unity/Assets/Scripts/Editor/ExportCsvEditorExtensions.cs
@@ -0,0 +1,53 @@
+﻿using CalorieCounter.ScaleEntries;
+using CalorieCounter.Utilities;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CalorieCounter.EditorExtensions
+{
+    public static class ExportCsvEditorExtensions
+    {
+
+        private const string MenuItemDirectory = @"Calorie Counter/Export CSV/";
+
+        [MenuItem(MenuItemDirectory + "Scale Entries")]
+        public static void ExportCsvScaleEntries()
+        {
+            if (Application.isPlaying)
+                return;
+
+            var scaleEntries = JsonConverter.Import<SortedList<DateTime, ScaleEntry>>(GlobalPaths.JsonScaleEntriesFileName, true);
+            var records = new List<ScaleEntryRecord>();
+            foreach (var scaleEntry in scaleEntries.Values)
+            {
+                records.Add(new ScaleEntryRecord(scaleEntry));
+            }
+
+            var filePath = GetFullCsvFilePath(GlobalPaths.CsvScaleEntriesFileName);
+            File.WriteAllText(filePath, string.Empty);
+
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(records);
+            }
+            Debug.Log($"New records written to {filePath}");
+        }
+
+        private static string GetFullCsvFilePath(string filePath)
+        {
+            string fullCsvFilePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), GlobalPaths.CsvDirectoryName, filePath));
+            string fullCsvFilePathDir = Path.GetDirectoryName(fullCsvFilePath);
+
+            if (!Directory.Exists(fullCsvFilePathDir))
+            {
+                Directory.CreateDirectory(fullCsvFilePathDir);
+            }
+            return fullCsvFilePath;
+        }
+    }
+}
