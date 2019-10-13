7190e832315243ccf85a1ef71c3a854ec5621dae
PatrickManalich <patrickmanalich@gmail.com>
2019-10-12 21:19:05 -0400

    JsonUpdater for updating rice white meal source

--- Unity/Assets/Scripts/Editor/MiscellaneousEditorExtensions.cs
+++ Unity/Assets/Scripts/Editor/MiscellaneousEditorExtensions.cs
@@ -1,4 +1,10 @@
-﻿using UnityEditor;
+﻿using CalorieCounter.MealEntries;
+using CalorieCounter.MealSources;
+using CalorieCounter.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
@@ -28,7 +34,44 @@ namespace CalorieCounter.EditorExtensions
             if (Application.isPlaying)
                 return;
 
-            Debug.LogError("JSON Updater hasn't been implemented");
+            var riceWhiteId = "43ff7c5d-b870-40f9-90a8-c55a7afa3305";
+            var newFat = 0f;
+            var newCarbs = 144f;
+            var newProtein = 12f;
+
+            var mealEntries = JsonConverter.ImportMealEntries(true);
+            foreach(var mealEntry in mealEntries.Values)
+            {
+                var mealProportionsDictionary = mealEntry.mealProportionsDictionary;
+                foreach (MealSourceType mealSourceType in Enum.GetValues(typeof(MealSourceType)))
+                {
+                    if (!mealProportionsDictionary.ContainsKey(mealSourceType))
+                        continue;
+
+                    var oldMealProportions = mealProportionsDictionary[mealSourceType];
+                    var newMealProportions = new List<MealProportion>();
+                    foreach(var oldMealProportion in oldMealProportions)
+                    {
+                        var oldMealSource = oldMealProportion.mealSource;
+                        if(oldMealSource.id == riceWhiteId)
+                        {
+                            var newMealSource = new MealSource(oldMealSource.servingSize, newFat, newCarbs, newProtein, oldMealSource.description, oldMealSource.mealSourceType)
+                            {
+                                id = oldMealSource.id
+                            };
+                            var newMealProportion = new MealProportion(oldMealProportion.servingAmount, newMealSource);
+                            newMealProportions.Add(newMealProportion);
+                        }
+                        else
+                        {
+                            newMealProportions.Add(oldMealProportion);
+                        }
+                    }
+                    mealEntry.mealProportionsDictionary[mealSourceType] = newMealProportions;
+                    JsonConverter.ExportFile(mealEntry, JsonConverter.GetMealEntryPath(mealEntry.dateTime), true);
+                }
+            }
+            Debug.Log("JSON Updater ran successfully");
         }
     }
 }
