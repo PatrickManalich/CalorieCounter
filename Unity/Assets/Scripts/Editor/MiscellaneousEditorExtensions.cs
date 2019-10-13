26e86f0583d09c2271d102368912e2fb77d873ce
PatrickManalich <patrickmanalich@gmail.com>
2019-10-12 21:47:14 -0400

    Remove JsonUpdater functionality

--- Unity/Assets/Scripts/Editor/MiscellaneousEditorExtensions.cs
+++ Unity/Assets/Scripts/Editor/MiscellaneousEditorExtensions.cs
@@ -1,13 +1,6 @@
-﻿using CalorieCounter.Utilities;
-using System;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using OldScaleEntries = CalorieCounterOld.ScaleEntries;
-using NewScaleEntries = CalorieCounter.ScaleEntries;
-using OldTargetEntries = CalorieCounterOld.TargetEntries;
-using NewTargetEntries = CalorieCounter.TargetEntries;
 
 
 namespace CalorieCounter.EditorExtensions
@@ -36,26 +29,7 @@ namespace CalorieCounter.EditorExtensions
             if (Application.isPlaying)
                 return;
 
-            var oldScaleEntries = JsonConverter.ImportFile<SortedList<DateTime, OldScaleEntries.ScaleEntry>>(GlobalPaths.JsonScaleEntriesFileName, true);
-            var newScaleEntries = new SortedList<DateTime, NewScaleEntries.ScaleEntry>();
-            foreach (var oldScaleEntry in oldScaleEntries.Values)
-            {
-                var newScaleEntry = new NewScaleEntries.ScaleEntry(oldScaleEntry.date, oldScaleEntry.weight, oldScaleEntry.bodyFat,
-                    oldScaleEntry.bodyWater, oldScaleEntry.muscleMass, oldScaleEntry.boneMass, oldScaleEntry.bmi);
-                newScaleEntries.Add(newScaleEntry.dateTime, newScaleEntry);
-            }
-            JsonConverter.ExportFile(newScaleEntries, GlobalPaths.JsonScaleEntriesFileName, true);
-
-
-            var oldTargetEntries = JsonConverter.ImportFile<SortedList<DateTime, OldTargetEntries.TargetEntry>>(GlobalPaths.JsonTargetEntriesFileName, true);
-            var newTargetEntries = new SortedList<DateTime, NewTargetEntries.TargetEntry>();
-            foreach (var oldTargetEntry in oldTargetEntries.Values)
-            {
-                var newTargetEntry = new NewTargetEntries.TargetEntry(oldTargetEntry.date, oldTargetEntry.weight);
-                newTargetEntries.Add(newTargetEntry.dateTime, newTargetEntry);
-            }
-            JsonConverter.ExportFile(newTargetEntries, GlobalPaths.JsonTargetEntriesFileName, true);
-            Debug.Log("JSON Updater ran successfully");
+            Debug.LogWarning("JSON Updater not implemented");
         }
     }
 }
