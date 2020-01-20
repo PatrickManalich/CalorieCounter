279686a5d68a8befd57aa0638d8806139ff69905
PatrickManalich <patrickmanalich@gmail.com>
2020-01-20 00:07:38 -0500

    Create DayMealPattern and add one

---
+++ Unity/Assets/Scripts/MealEntries/MealPatterns/DayTypeMealPattern.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CalorieCounter.MealEntries.MealPatterns
+{
+
+    [CreateAssetMenu(fileName = "DayTypeMealPattern", menuName = "Calorie Counter/Day Type Meal Pattern")]
+    public class DayTypeMealPattern : ScriptableObject
+    {
+        public DayType dayType;
+        public MealSourceType mealSourceType;
+        public float servingAmount;
+        public string mealSourceId;
+    }
+}
+
+
