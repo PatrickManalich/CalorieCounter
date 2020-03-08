6eb744ba141dcdbc32fc0091f3def97f4d197673
PatrickManalich <patrickmanalich@gmail.com>
2020-03-08 15:29:47 -0400

    Create MealPatternValidationChecker

---
+++ Unity/Assets/Scripts/Editor/MealPatternValidationChecker.cs
@@ -0,0 +1,48 @@
+﻿using CalorieCounter.MealEntries;
+using CalorieCounter.MealEntries.MealPatterns;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace CalorieCounter.EditorExtensions
+{
+    [InitializeOnLoad]
+	public static class MealPatternValidationChecker
+	{
+        private static readonly string DayMealPatternsPath = Path.Combine(GlobalPaths.ScriptableObjectsDirectoryName, GlobalPaths.DayMealPatternsDirectoryName);
+        private static readonly string DayTypeMealPatternsPath = Path.Combine(GlobalPaths.ScriptableObjectsDirectoryName, GlobalPaths.DayTypeMealPatternsDirectoryName);
+
+        static MealPatternValidationChecker()
+		{
+            EditorApplication.playModeStateChanged += EditorApplication_OnPlayModeStateChanged;
+		}
+
+        private static void EditorApplication_OnPlayModeStateChanged(PlayModeStateChange playModeStateChange)
+        {
+            if (playModeStateChange == PlayModeStateChange.EnteredPlayMode)
+            {
+                var dayMealPatterns = Resources.LoadAll(DayMealPatternsPath, typeof(DayMealPattern)).Cast<DayMealPattern>().ToList();
+                foreach (var dayMealPattern in dayMealPatterns)
+                {
+                    CheckIfMealPatternHasBeenValidated(dayMealPattern.name, dayMealPattern.mealSuggestion);
+                }
+
+                var dayTypeMealPatterns = Resources.LoadAll(DayTypeMealPatternsPath, typeof(DayTypeMealPattern)).Cast<DayTypeMealPattern>().ToList();
+                foreach (var dayTypeMealPattern in dayTypeMealPatterns)
+                {
+                    CheckIfMealPatternHasBeenValidated(dayTypeMealPattern.name, dayTypeMealPattern.mealSuggestion);
+                }
+            }
+        }
+
+        private static void CheckIfMealPatternHasBeenValidated(string mealPatternName, MealSuggestion mealSuggestion)
+        {
+            if (mealSuggestion.mealProportion.calories <= 0)
+            {
+                Debug.LogWarning($"Meal pattern: {mealPatternName} has zero calories, make sure you've validated");
+            }
+        }
+	}
+}
+
