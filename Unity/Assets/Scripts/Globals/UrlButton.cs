ca85bd35537b761ff9e5c0d734494654d58a23a1
PatrickManalich <patrickmanalich@gmail.com>
2020-05-23 14:55:02 -0400

    [cosmetic] Rename field to match naming convention

--- Unity/Assets/Scripts/Globals/UrlButton.cs
+++ Unity/Assets/Scripts/Globals/UrlButton.cs
@@ -8,19 +8,19 @@ namespace CalorieCounter
     public class UrlButton : MonoBehaviour
     {
         [SerializeField]
-        private string _Url = default;
+        private string _url = default;
 
         private Button _button;
 
         private void Start()
         {
             _button = GetComponent<Button>();
-            _button.onClick.AddListener(() => Application.OpenURL(_Url));
+            _button.onClick.AddListener(() => Application.OpenURL(_url));
         }
 
         private void OnDestroy()
         {
-            _button.onClick.RemoveListener(() => Application.OpenURL(_Url));
+            _button.onClick.RemoveListener(() => Application.OpenURL(_url));
         }
     }
 
