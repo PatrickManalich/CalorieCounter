0203146ca22405ca65e57917385f1aef6bade16b
PatrickManalich <patrickmanalich@gmail.com>
2020-05-24 23:12:37 -0400

    ExportButton sets interactability based on existing differences

--- Unity/Assets/Scripts/Globals/ExportButton.cs
+++ Unity/Assets/Scripts/Globals/ExportButton.cs
@@ -11,6 +11,9 @@ namespace CalorieCounter
         [SerializeField]
         private List<AbstractAdapter> _adapters = default;
 
+        [SerializeField]
+        private List<ScrollView> _scrollViews = default;
+
         private Button _button;
 
         private void Start()
@@ -20,15 +23,37 @@ namespace CalorieCounter
             {
                 _button.onClick.AddListener(adapter.Export);
             }
+            foreach(var scrollView in _scrollViews)
+            {
+                scrollView.RowChanged += RefreshButtonInteractability;
+            }
+            _button.interactable = false;
         }
 
         private void OnDestroy()
         {
+            foreach (var scrollView in _scrollViews)
+            {
+                scrollView.RowChanged -= RefreshButtonInteractability;
+            }
             foreach (var adapter in _adapters)
             {
                 _button.onClick.RemoveListener(adapter.Export);
             }
         }
-    }
 
+        private void RefreshButtonInteractability()
+        {
+            var doDifferencesExist = false;
+            foreach (var adapter in _adapters)
+            {
+                if (adapter.DoDifferencesExist())
+                {
+                    doDifferencesExist = true;
+                    break;
+                }
+            }
+            _button.interactable = doDifferencesExist;
+        }
+    }
 }
