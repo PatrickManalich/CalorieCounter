44630a9116bd4f2fcde489a328c24eb241843e83
PatrickManalich <patrickmanalich@gmail.com>
2020-06-27 13:54:43 -0400

    Create AssetEditorExtensions

---
+++ Unity/Assets/Scripts/Editor/AssetEditorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace CalorieCounter.EditorExtensions
+{
+    public static class AssetEditorExtensions
+    {
+        private const string MenuItemDirectory = @"Calorie Counter/Assets/";
+
+        [MenuItem(MenuItemDirectory + "Force Reserialize Selected Assets")]
+        public static void ForceReserializeSelectedAssets()
+        {
+            var assetPaths = new List<string>();
+            foreach (var obj in Selection.objects)
+            {
+                var assetPath = AssetDatabase.GetAssetPath(obj);
+                assetPaths.Add(assetPath);
+            }
+            AssetDatabase.ForceReserializeAssets(assetPaths);
+            Debug.Log($"Force reserialized {assetPaths.Count} asset(s)");
+        }
+    }
+}
