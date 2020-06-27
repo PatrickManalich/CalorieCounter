61da0724df5575e868142d0ea7f27245ece9168e
PatrickManalich <patrickmanalich@gmail.com>
2020-06-27 17:42:46 -0400

    Create CustomScrollRect

---
+++ Unity/Assets/Scripts/Globals/CustomScrollRect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CalorieCounter
+{
+    public class CustomScrollRect : ScrollRect
+    {
+        private const float MinScrollbarSize = 0.3f;
+
+        public override void Rebuild(CanvasUpdate executing)
+        {
+            base.Rebuild(executing);
+            if (horizontalScrollbar)
+            {
+                horizontalScrollbar.size = Mathf.Max(MinScrollbarSize, horizontalScrollbar.size);
+            }
+            if (verticalScrollbar)
+            {
+                verticalScrollbar.size = Mathf.Max(MinScrollbarSize, verticalScrollbar.size);
+            }
+        }
+
+        protected override void LateUpdate()
+        {
+            base.LateUpdate();
+            if (horizontalScrollbar)
+            {
+                horizontalScrollbar.size = Mathf.Max(MinScrollbarSize, horizontalScrollbar.size);
+            }
+            if (verticalScrollbar)
+            {
+                verticalScrollbar.size = Mathf.Max(MinScrollbarSize, verticalScrollbar.size);
+            }
+        }
+    }
+}
