6991dd879d4419d4467c2c526ff33d0c45f3a64d
PatrickManalich <patrickmanalich@gmail.com>
2020-06-28 09:28:38 -0400

    Remove setters in CsvWriterRecords

--- Unity/Assets/Scripts/Editor/CsvWriterRecords.cs
+++ Unity/Assets/Scripts/Editor/CsvWriterRecords.cs
@@ -8,13 +8,13 @@ namespace CalorieCounter.EditorExtensions
 
     public class ScaleEntryRecord
     {
-        public string Date { get; set; }
-        public float Weight { get; set; }
-        public float BodyFat { get; set; }
-        public float BodyWater { get; set; }
-        public float MuscleMass { get; set; }
-        public float BoneMass { get; set; }
-        public float Bmi { get; set; }
+        public string Date { get; }
+        public float Weight { get; }
+        public float BodyFat { get; }
+        public float BodyWater { get; }
+        public float MuscleMass { get; }
+        public float BoneMass { get; }
+        public float Bmi { get; }
 
         public ScaleEntryRecord(ScaleEntry scaleEntry)
         {
@@ -31,17 +31,17 @@ namespace CalorieCounter.EditorExtensions
 
     public class TargetEntryRecord
     {
-        public string Date { get; set; }
-        public float Weight { get; set; }
-        public float CalorieMaintenanceLevel { get; set; }
-        public float RestDayFat { get; set; }
-        public float RestDayCarbs { get; set; }
-        public float RestDayProtein { get; set; }
-        public float RestDayCalories { get; set; }
-        public float TrainingDayFat { get; set; }
-        public float TrainingDayCarbs { get; set; }
-        public float TrainingDayProtein { get; set; }
-        public float TrainingDayCalories { get; set; }
+        public string Date { get; }
+        public float Weight { get; }
+        public float CalorieMaintenanceLevel { get; }
+        public float RestDayFat { get; }
+        public float RestDayCarbs { get; }
+        public float RestDayProtein { get; }
+        public float RestDayCalories { get; }
+        public float TrainingDayFat { get; }
+        public float TrainingDayCarbs { get; }
+        public float TrainingDayProtein { get; }
+        public float TrainingDayCalories { get; }
 
         public TargetEntryRecord(TargetEntry targetEntry)
         {
@@ -61,17 +61,17 @@ namespace CalorieCounter.EditorExtensions
 
     public class ResultRecord
     {
-        public string Date { get; set; }
-        public string DayType { get; set; }
-        public string DayOfWeek { get; set; }
-        public float TotalFat { get; set; }
-        public float TargetFat { get; set; }
-        public float TotalCarbs { get; set; }
-        public float TargetCarbs { get; set; }
-        public float TotalProtein { get; set; }
-        public float TargetProtein { get; set; }
-        public float TotalCalories { get; set; }
-        public float TargetCalories { get; set; }
+        public string Date { get; }
+        public string DayType { get; }
+        public string DayOfWeek { get; }
+        public float TotalFat { get; }
+        public float TargetFat { get; }
+        public float TotalCarbs { get; }
+        public float TargetCarbs { get; }
+        public float TotalProtein { get; }
+        public float TargetProtein { get; }
+        public float TotalCalories { get; }
+        public float TargetCalories { get; }
 
         public ResultRecord(MealEntry mealEntry, TargetEntry targetEntry)
         {
