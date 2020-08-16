e5f2192881b8c85be64124c4e45a87435003c905
PatrickManalich <patrickmanalich@gmail.com>
2020-08-16 08:06:10 -0400

    Create InputUtilities script

---
+++ Unity/Assets/Scripts/Utilities/InputUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace CalorieCounter.Utilities
+{
+    public static class InputUtilities
+    {
+        private static readonly char[] ValidSpecialChars = new char[]
+        {
+                        '-', '\'', '&', '.', ' ', '/', '%',
+        };
+
+        // Unused parameters are necessary for TMP_InputField.OnValidateInput()
+        public static char ValidateNonDecimalInput(string text, int charIndex, char addedChar)
+        {
+            return char.IsLetterOrDigit(addedChar) || ValidSpecialChars.Contains(addedChar) ? addedChar : '\0';
+        }
+    }
+}
