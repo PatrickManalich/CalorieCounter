1af2c2521a13a85036e8ee9ec50fa801c6e7b8f1
PatrickManalich <patrickmanalich@gmail.com>
2020-08-18 21:23:01 -0400

    Create TimeScaleEditor script

---
+++ Unity/Assets/Scripts/Editor/Windows/TimeScaleEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CalorieCounter.EditorExtensions
+{
+    public class TimeScaleEditorWindow : EditorWindow
+    {
+        private const int DefaultMinTimeScale = 0;
+        private const int DefaultMaxTimeScale = 100;
+
+        private float minTimeScale = DefaultMinTimeScale;
+        private float maxTimeScale = DefaultMaxTimeScale;
+
+        [MenuItem("Calorie Counter/Window/Time Scale Editor")]
+        public static void ShowWindow()
+        {
+            GetWindow<TimeScaleEditorWindow>("Time Scale Editor");
+        }
+
+        private void OnGUI()
+        {
+            Time.timeScale = EditorGUILayout.Slider("Time Scale", Time.timeScale, minTimeScale, maxTimeScale);
+            MinTimeScale = EditorGUILayout.FloatField("Min Time Scale", minTimeScale);
+            MaxTimeScale = EditorGUILayout.FloatField("Max Time Scale", maxTimeScale);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Default"))
+            {
+                minTimeScale = DefaultMinTimeScale;
+                maxTimeScale = DefaultMaxTimeScale;
+                Time.timeScale = 1;
+            }
+            if (GUILayout.Button("Min Speed"))
+            {
+                Time.timeScale = minTimeScale;
+            }
+            if (GUILayout.Button("Max Speed"))
+            {
+                Time.timeScale = maxTimeScale;
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        private float MinTimeScale {
+            set {
+                minTimeScale = value;
+                if (minTimeScale < DefaultMinTimeScale)
+                {
+                    minTimeScale = DefaultMinTimeScale;
+                }
+            }
+        }
+
+        private float MaxTimeScale {
+            set {
+                maxTimeScale = value;
+                if (maxTimeScale > DefaultMaxTimeScale)
+                {
+                    maxTimeScale = DefaultMaxTimeScale;
+                }
+            }
+        }
+    }
+}
