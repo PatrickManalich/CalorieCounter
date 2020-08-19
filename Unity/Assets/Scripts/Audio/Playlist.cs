3ebab18ae0366c5c00da8c46ae9ef9209746e7a4
PatrickManalich <patrickmanalich@gmail.com>
2020-08-18 20:50:52 -0400

    Create Playlist script

---
+++ Unity/Assets/Scripts/Audio/Playlist.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CalorieCounter.Audio
+{
+    [CreateAssetMenu(fileName = "DayMealPattern", menuName = "Calorie Counter/Playlist")]
+    public class Playlist : ScriptableObject
+    {
+        public AudioClip[] audioClips;
+    }
+}
