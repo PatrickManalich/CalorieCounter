79671dfdc450f4e67faeeff2399774f34e660747
PatrickManalich <patrickmanalich@gmail.com>
2020-08-22 19:36:28 -0400

    Create ClickSoundEffectPlayer script

---
+++ Unity/Assets/Scripts/Audio/ClickSoundEffectPlayer.cs
@@ -0,0 +1,30 @@
+﻿using CalorieCounter.Managers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace CalorieCounter.Audio
+{
+    [RequireComponent(typeof(Selectable))]
+    public class ClickSoundEffectPlayer : MonoBehaviour, IPointerDownHandler
+    {
+        private Selectable _selectable;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_selectable.interactable)
+            {
+                GameManager.SoundEffectsManager.PlaySuccessfulClickSoundEffect();
+            }
+            else
+            {
+                GameManager.SoundEffectsManager.PlayFailedClickSoundEffect();
+            }
+        }
+
+        private void Awake()
+        {
+            _selectable = GetComponent<Selectable>();
+        }
+    }
+}
