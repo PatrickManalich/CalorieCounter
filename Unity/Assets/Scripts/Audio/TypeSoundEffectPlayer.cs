39b8bca3b3421624b7ef28cbc58da59c7b395d4d
PatrickManalich <patrickmanalich@gmail.com>
2020-08-22 23:29:24 -0400

    Create TypeSoundEffectPlayer script

---
+++ Unity/Assets/Scripts/Audio/TypeSoundEffectPlayer.cs
@@ -0,0 +1,25 @@
+﻿using CalorieCounter.Managers;
+using TMPro;
+using UnityEngine;
+
+namespace CalorieCounter.Audio
+{
+    [RequireComponent(typeof(TMP_InputField))]
+    public class TypeSoundEffectPlayer : MonoBehaviour
+    {
+        private TMP_InputField _inputField;
+
+        private void Awake()
+        {
+            _inputField = GetComponent<TMP_InputField>();
+        }
+
+        private void Update()
+        {
+            if (_inputField.isFocused && Input.anyKeyDown)
+            {
+                GameManager.SoundEffectsManager.PlayTypeSoundEffect();
+            }
+        }
+    }
+}
