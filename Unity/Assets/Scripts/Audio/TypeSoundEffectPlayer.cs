e0cf98eb4cd3204dfdde19bfcc6d148641284638
PatrickManalich <patrickmanalich@gmail.com>
2020-08-22 23:29:54 -0400

    Add TypeSoundEffectPlayer to input fields

--- Unity/Assets/Scripts/Audio/TypeSoundEffectPlayer.cs
+++ Unity/Assets/Scripts/Audio/TypeSoundEffectPlayer.cs
@@ -1,22 +1,14 @@
 ﻿using CalorieCounter.Managers;
-using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CalorieCounter.Audio
 {
-    [RequireComponent(typeof(TMP_InputField))]
     public class TypeSoundEffectPlayer : MonoBehaviour
     {
-        private TMP_InputField _inputField;
-
-        private void Awake()
-        {
-            _inputField = GetComponent<TMP_InputField>();
-        }
-
         private void Update()
         {
-            if (_inputField.isFocused && Input.anyKeyDown)
+            if (EventSystem.current.currentSelectedGameObject != null && EventSystem.current.currentSelectedGameObject == gameObject && Input.anyKeyDown)
             {
                 GameManager.SoundEffectsManager.PlayTypeSoundEffect();
             }
