4e3b3a7def071322d7f2ffe1ba79e0dfcf17b109
PatrickManalich <patrickmanalich@gmail.com>
2020-08-23 10:28:22 -0400

    Create PressInputKeySoundEffectPlayer script

---
+++ Unity/Assets/Scripts/Audio/PressInputKeySoundEffectPlayer.cs
@@ -0,0 +1,27 @@
+﻿using CalorieCounter.Managers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CalorieCounter.Audio
+{
+    public class PressInputKeySoundEffectPlayer : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameManager.InputKeyManager.InputKeyPressed += InputKeyManager_OnInputKeyPressed;
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.InputKeyManager.InputKeyPressed -= InputKeyManager_OnInputKeyPressed;
+        }
+
+        private void InputKeyManager_OnInputKeyPressed(object sender, InputKeyManager.InputKeyPressedEventArgs e)
+        {
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                GameManager.SoundEffectsManager.PlayPressInputKeySoundEffect();
+            }
+        }
+    }
+}
