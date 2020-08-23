57bd0a3f2020be77ddbb58cac4b177fc9716b41a
PatrickManalich <patrickmanalich@gmail.com>
2020-08-23 14:12:52 -0400

    Create AttainTargetSoundEffectPlayer

---
+++ Unity/Assets/Scripts/Audio/AttainTargetSoundEffectPlayer.cs
@@ -0,0 +1,39 @@
+﻿using CalorieCounter.Managers;
+using CalorieCounter.MealEntries;
+using UnityEngine;
+
+namespace CalorieCounter.Audio
+{
+    [RequireComponent(typeof(Totals))]
+    public class AttainTargetSoundEffectPlayer : MonoBehaviour
+    {
+        private Totals _totals;
+        private bool _targetAttained;
+
+        private void Start()
+        {
+            _totals = GetComponent<Totals>();
+
+            _totals.TargetAttained += Totals_OnTargetAttained;
+        }
+
+        private void OnDestroy()
+        {
+            _totals.TargetAttained -= Totals_OnTargetAttained;
+        }
+
+        private void Update()
+        {
+            if(_targetAttained)
+            {
+                GameManager.SoundEffectsManager.PlayAttainTargetSoundEffect();
+                _targetAttained = false;
+            }
+        }
+
+        private void Totals_OnTargetAttained(object sender, System.EventArgs e)
+        {
+            _targetAttained = true;
+        }
+    }
+}
